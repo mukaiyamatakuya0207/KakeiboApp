8e2026184877f189af7de3352d208201414545f9
TAKUYA MUKAIYAMA <mukaiyama.takuya.0207@gmail.com>
2026-02-01 09:48:22 +0900

    Initial Commit

---
+++ KakeiboApp/KakeiboAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakeiboAppApp.swift
+//  KakeiboApp
+//
+//  Created by TAKUYA MUKAIYAMA on 2026/02/01.
+//
+
+import SwiftUI
+
+@main
+struct KakeiboAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
